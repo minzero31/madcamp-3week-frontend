a88ed1b4009c665babb0d23d87028ddf26d590b3
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2024-07-13 13:10:02 +0900

    Main 화면#8

--- src/page/Loading.js
+++ src/page/Loading.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Lottie from "lottie-react";
+import loadingLottie from "../lottie/loadingLottie.json";
+import { useNavigate } from "react-router-dom";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/login");
+    }, 3000);
+  }, []);
+
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          width: "250px",
+          height: "250px",
+          backgroundColor: "#f5f5f5",
+          borderRadius: "50%",
+          padding: "1px",
+        }}
+      >
+        <Lottie animationData={loadingLottie} />
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
